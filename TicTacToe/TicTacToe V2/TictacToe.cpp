dcdbacbc8097507f900a1cf56992afad5dc47e9e
LadoLettus <78886542+LadoLettus@users.noreply.github.com>
2022-07-12 20:30:18 -0500

    Add files via upload

---
+++ TicTacToe/TicTacToe V2/TictacToe.cpp
@@ -0,0 +1,231 @@
+#include <iostream>
+using namespace std;
+
+//This char variable creates a 3x3 board numbered 1-9
+char matrix[3][3] = { '1','2','3' ,'4' ,'5' ,'6' ,'7' ,'8','9' };
+//This sets teh player as X, it is switched later in another funciton
+char player = 'X';
+int n;
+void Draw()
+{
+	system("cls");
+	cout << "---TIC TAC TOE---" << endl;
+	cout << "" << endl;
+	//This for loop draws the board defined in the matrix char
+	for (int i = 0; i < 3; i++)
+	{
+		for (int j = 0; j < 3; j++)
+		{
+			cout << matrix[i][j] << " ";
+		}
+		cout << endl;
+	}
+
+}
+void Input()
+{
+	int a;
+	if (player == 'X')
+	{
+		cout << "" << endl;
+		cout << "X input a number: ";
+	}
+	else if (player == 'O')
+	{
+		cout << "" << endl;
+		cout << "O input a number: ";
+	}
+	cin >> a;
+
+	//This if else sequence puts the "player" (X or O) into the point in the matrix char that is selected (1-9)
+	if (a == 1)
+	{
+		if (matrix[0][0] == '1')
+			matrix[0][0] = player;
+		else
+		{
+			cout << "" << endl;
+			cout << "Field is already in use try again!" << endl;
+			Input();
+		}
+	}
+	if (a == 2)
+	{
+		if (matrix[0][1] == '2')
+			matrix[0][1] = player;
+		else
+		{
+			cout << "" << endl;
+			cout << "Field is already in use try again!" << endl;
+			Input();
+		}
+	}
+	if (a == 3)
+	{
+		if (matrix[0][2] == '3')
+			matrix[0][2] = player;
+		else
+		{
+			cout << "" << endl;
+			cout << "Field is already in use try again!" << endl;
+			Input();
+		}
+	}
+	if (a == 4)
+	{
+		if (matrix[1][0] == '4')
+			matrix[1][0] = player;
+		else
+		{
+			cout << "" << endl;
+			cout << "Field is already in use try again!" << endl;
+			Input();
+		}
+	}
+	if (a == 5)
+	{
+		if (matrix[1][1] == '5')
+			matrix[1][1] = player;
+		else
+		{
+			cout << "" << endl;
+			cout << "Field is already in use try again!" << endl;
+			Input();
+		}
+	}
+	if (a == 6)
+	{
+		if (matrix[1][2] == '6')
+			matrix[1][2] = player;
+		else
+		{
+			cout << "" << endl;
+			cout << "Field is already in use try again!" << endl;
+			Input();
+		}
+	}
+	if (a == 7)
+	{
+		if (matrix[2][0] == '7')
+			matrix[2][0] = player;
+		else
+		{
+			cout << "" << endl;
+			cout << "Field is already in use try again!" << endl;
+			Input();
+		}
+	}
+	if (a == 8)
+	{
+		if (matrix[2][1] == '8')
+			matrix[2][1] = player;
+		else
+		{
+			cout << "" << endl;
+			cout << "Field is already in use try again!" << endl;
+			Input();
+		}
+	}
+	if (a == 9)
+	{
+		if (matrix[2][2] == '9')
+			matrix[2][2] = player;
+		else
+		{
+			cout << "" << endl;
+			cout << "Field is already in use try again!" << endl;
+			Input();
+		}
+	}
+
+}
+void TogglePlayer()
+//This if else swaps the player from X to Y in between turns 
+{
+	if (player == 'X')
+		player = 'O';
+	else
+		player = 'X';
+}
+char Win()
+{
+	//This fuction checks for win conditions columns, rows, and diagonals for X and O
+	//columns X
+	if (matrix[0][0] == 'X' && matrix[0][1] == 'X' && matrix[0][2] == 'X')
+		return 'X';
+	if (matrix[1][0] == 'X' && matrix[1][1] == 'X' && matrix[1][2] == 'X')
+		return 'X';
+	if (matrix[2][0] == 'X' && matrix[2][1] == 'X' && matrix[2][2] == 'X')
+		return 'X';
+	//rows X
+	if (matrix[0][0] == 'X' && matrix[1][0] == 'X' && matrix[2][0] == 'X')
+		return 'X';
+	if (matrix[0][1] == 'X' && matrix[1][1] == 'X' && matrix[2][1] == 'X')
+		return 'X';
+	if (matrix[0][1] == 'X' && matrix[1][1] == 'X' && matrix[2][1] == 'X')
+		return 'X';
+	//diagonal X
+	if (matrix[0][0] == 'X' && matrix[1][1] == 'X' && matrix[2][2] == 'X')
+		return 'X';
+	if (matrix[0][2] == 'X' && matrix[1][1] == 'X' && matrix[2][0] == 'X')
+		return 'X';
+
+	//columns O
+	if (matrix[0][0] == 'O' && matrix[0][1] == 'O' && matrix[0][2] == 'O')
+		return 'O';
+	if (matrix[1][0] == 'O' && matrix[1][1] == 'O' && matrix[1][2] == 'O')
+		return 'O';
+	if (matrix[2][0] == 'O' && matrix[2][1] == 'O' && matrix[2][2] == 'O')
+		return 'O';
+	//rows O
+	if (matrix[0][0] == 'O' && matrix[1][0] == 'O' && matrix[2][0] == 'O')
+		return 'O';
+	if (matrix[0][1] == 'O' && matrix[1][1] == 'O' && matrix[2][1] == 'O')
+		return 'O';
+	if (matrix[0][1] == 'O' && matrix[1][1] == 'O' && matrix[2][1] == 'O')
+		return 'O';
+	//diagonal O
+	if (matrix[0][0] == 'O' && matrix[1][1] == 'O' && matrix[2][2] == 'O')
+		return 'O';
+	if (matrix[0][2] == 'O' && matrix[1][1] == 'O' && matrix[2][0] == 'O')
+		return 'O';
+
+	return '/';
+}
+int main()
+{
+	n = 0;
+	//Draw board
+	Draw();
+	//This while function runs while true 
+	//It runs the Input function for player to input their number
+	//Then draws the board with the player input 
+	//Then checks for win conditions 
+	//Then toggles player
+	while (1)
+	{
+		Input();
+		n++;
+		Draw();
+		if (Win() == 'X')
+		{
+			cout << "" << endl;
+			cout << "X WINS!";
+			break;
+		}
+		else if (Win() == 'O')
+		{
+			cout << "" << endl;
+			cout << "O WINS!";
+			break;
+		}
+		else if (Win() == '/' && n == 9)
+		{
+			cout << "" << endl;
+			cout << "TIE!";
+			break;
+		}
+		TogglePlayer();
+	}
+	system("pause>0");
+}
